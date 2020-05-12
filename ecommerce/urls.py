b6abbe4dc50f7b4f662c8cbecaf277bd00a8ddf9
anilchoudhary <masteranilchoudhary@gmail.com>
2020-05-12 22:30:23 +0530

    added html forms

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page, about_page, contact_page
+from .views import home_page, about_page, contact_page, shop_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
     path('about/', about_page),
     path('contact/', contact_page),
+    path('shop/', shop_page),
 
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -23,4 +23,17 @@ def contact_page(request):
         "title": "contact_page",
         "content": "welcome to the contact page"
     }
+    if request.method == "POST":
+        # print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
+
+
+def shop_page(request):
+    context = {
+    "title": "shop page",
+    "content": "welcome to the shop page"
+    }
     return render(request, "home_page.html", context)
