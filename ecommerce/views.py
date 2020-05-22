f378fc2fc37a797c45d1d84e6e7807adc898f87c
anilchoudhary <masteranilchoudhary@gmail.com>
2020-05-22 10:45:18 +0530

    done django-forms

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import ContactForm
+
 
 def home_page(request):
     context = {
@@ -19,21 +21,25 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contact page",
-        "content": "Welcome to the contact page"
+        "content": "Welcome to the contact page",
+        "form": contact_form
     }
-    if request.method == "POST":
-        # print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     # print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
 
 def shop_page(request):
     context = {
-    "title": "Shop page",
-    "content": "Welcome to the shop page"
+        "title": "Shop page",
+        "content": "Welcome to the shop page"
     }
     return render(request, "home_page.html", context)
